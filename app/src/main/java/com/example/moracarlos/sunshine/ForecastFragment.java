74aed1e8a59e91bacd24544908330a0a4be332d4
moracarlos <moracarlos9@gmail.com>
2015-05-10 19:06:52 -0430

    Agregada la segunda actiidad

--- app/src/main/java/com/example/moracarlos/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/moracarlos/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.moracarlos.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -65,7 +67,15 @@ public class ForecastFragment extends Fragment {
         mAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_tem_forecast, R.id.list_item_forecast_textview, weekForecast);
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mAdapter);
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick (AdapterView < ? > adapterView, View view,int i, long l){
+                String forecast = mAdapter.getItem(i);
+                //Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent detailActivityIntent = new Intent(getActivity(), DetailActivity.class).putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(detailActivityIntent);
+            }
+        });
         return rootView;
     }
 
