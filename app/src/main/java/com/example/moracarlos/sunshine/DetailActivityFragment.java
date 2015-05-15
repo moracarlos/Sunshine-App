7fc6987624fb4f52196ae03dd756087aff1a27d0
moracarlos <moracarlos9@gmail.com>
2015-05-15 00:37:11 -0430

    ShareActionProvider

--- app/src/main/java/com/example/moracarlos/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/example/moracarlos/sunshine/DetailActivityFragment.java
@@ -3,10 +3,11 @@ package com.example.moracarlos.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.ActionProvider;
+import android.support.v4.view.MenuItemCompat;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -17,20 +18,24 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment {
 
-    private ActionProvider mShareActionProvider;
     private final static String HASHTAG = "#Sunshine";
-
+    private String message;
     public DetailActivityFragment() {
     }
 
 
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         Intent intent = getActivity().getIntent();
-        String message="";
+        message="";
         if (intent!=null && intent.hasExtra(Intent.EXTRA_TEXT)){
             message = intent.getStringExtra(Intent.EXTRA_TEXT);
             ((TextView) rootView.findViewById(R.id.detail_text)).setText(message);
@@ -40,6 +45,31 @@ public class DetailActivityFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecast_detail_menu, menu);
 
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        android.support.v7.widget.ShareActionProvider mShareActionProvider = (android.support.v7.widget.ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if (mShareActionProvider!=null) {
+            mShareActionProvider.setShareIntent(getDefaultShareIntent());
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id == R.id.action_share){
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private Intent getDefaultShareIntent()
+    {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, message+" "+HASHTAG);
+        return intent;
     }
 }
